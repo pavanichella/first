41f89cc1f089e6a70e6139e18f0fc342ad59be9b
pavanichella <92535444+pavanichella@users.noreply.github.com>
2021-10-14 22:11:15 +0530

    Delete Demo directory

--- Demo/src/com/java/pr/First.java
+++
@@ -1,9 +0,0 @@
-package com.java.pr;
-
-public class First {
- 
-	public static void main(String args[])
-	{
-		System.out.println("Hello world");
-	}
-}
--- Demo/src/com/java/pr/PP1.java
+++
@@ -1,5 +0,0 @@
-package com.java.pr;
-
-public class PP1 {
-
-}
